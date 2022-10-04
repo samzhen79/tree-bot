8c1224865d5ee759faea986dd8b385c584965c76
x74717zz <zhaojian.zhen@student.manchester.ac.uk>
2022-10-05 00:32:21 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import { Client, GatewayIntentBits } from 'discord.js';
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+client.on("messageCreate", async (message) => {
+    // console.log(message)
+
+    if (!message?.author.bot) {
+        console.log(message.content)
+        if (message.content == 'tree') {
+            message.channel.send({ files: [{ attachment: 'tree.png' }] })
+        }
+    }
+})
+
+client.login(process.env.DISCORD_TOKEN)
