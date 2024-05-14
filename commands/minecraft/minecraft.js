1c7d80b3a7a165a8e1e3caa1b63219e9b0e851bb
Sam Zhen <samjzhen79@gmail.com>
2024-05-14 02:40:32 +0100

    Fix playing music

--- commands/minecraft/minecraft.js
+++ commands/minecraft/minecraft.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { minecraftIP } = require('../config.json');
+const { minecraftIP } = require('../../config.json');
 
 module.exports = {
 	data: new SlashCommandBuilder()
